65330c487021f1d69a17d2fb8a3bbaa748cb736f
efekos <56752400+efekos@users.noreply.github.com>
2024-09-11 18:38:52 +0300

    add #register to ClaRecipeTypes

--- src/main/java/dev/efekos/cla/init/ClaRecipeTypes.java
+++ src/main/java/dev/efekos/cla/init/ClaRecipeTypes.java
@@ -4,6 +4,9 @@ import dev.efekos.cla.Main;
 import dev.efekos.cla.recipe.CuttingRecipe;
 import dev.efekos.cla.recipe.FryingRecipe;
 import dev.efekos.cla.recipe.PanningRecipe;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.RecipeType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -11,14 +14,14 @@ import net.minecraft.util.Identifier;
 public class ClaRecipeTypes {
 
     public static void run(){
-        Registry.register(Registries.RECIPE_TYPE, Identifier.of(Main.MOD_ID,"cutting"), CuttingRecipe.Type.INSTANCE);
-        Registry.register(Registries.RECIPE_SERIALIZER,Identifier.of(Main.MOD_ID,"cutting"), CuttingRecipe.Serializer.INSTANCE);
+        register("cutting",CuttingRecipe.Serializer.INSTANCE,CuttingRecipe.Type.INSTANCE);
+        register("frying",FryingRecipe.Serializer.INSTANCE,FryingRecipe.Type.INSTANCE);
+        register("panning",PanningRecipe.Serializer.INSTANCE,PanningRecipe.Type.INSTANCE);
+    }
 
-        Registry.register(Registries.RECIPE_TYPE,Identifier.of(Main.MOD_ID,"frying"), FryingRecipe.Type.INSTANCE);
-        Registry.register(Registries.RECIPE_SERIALIZER,Identifier.of(Main.MOD_ID,"frying"), FryingRecipe.Serializer.INSTANCE);
-
-        Registry.register(Registries.RECIPE_TYPE,Identifier.of(Main.MOD_ID,"panning"), PanningRecipe.Type.INSTANCE);
-        Registry.register(Registries.RECIPE_SERIALIZER,Identifier.of(Main.MOD_ID,"panning"), PanningRecipe.Serializer.INSTANCE);
+    private static <T extends Recipe<?>> void register(String id, RecipeSerializer<T> serializer, RecipeType<T> type) {
+        Registry.register(Registries.RECIPE_TYPE,Identifier.of(Main.MOD_ID,id), type);
+        Registry.register(Registries.RECIPE_SERIALIZER,Identifier.of(Main.MOD_ID,id), serializer);
     }
 
 }
