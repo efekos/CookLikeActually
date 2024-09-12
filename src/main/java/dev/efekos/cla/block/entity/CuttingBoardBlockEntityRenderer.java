aee4055c029af95192bcf7922a6b1bdb3af1073e
efekos <56752400+efekos@users.noreply.github.com>
2024-09-12 23:05:16 +0300

    fix CuttingBoardBlockEntityRenderer not rendering the block

--- src/main/java/dev/efekos/cla/block/entity/CuttingBoardBlockEntityRenderer.java
+++ src/main/java/dev/efekos/cla/block/entity/CuttingBoardBlockEntityRenderer.java
@@ -1,19 +1,31 @@
 package dev.efekos.cla.block.entity;
 
+import dev.efekos.cla.block.CuttingBoardBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.MaceItem;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationAxis;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
+
+import java.util.Locale;
 
 public class CuttingBoardBlockEntityRenderer implements BlockEntityRenderer<CuttingBoardBlockEntity> {
 
@@ -28,14 +40,29 @@ public class CuttingBoardBlockEntityRenderer implements BlockEntityRenderer<Cutt
         ItemStack item = entity.getItem();
         if(item==null)return;
 
+        World world = entity.getWorld();
+        BlockPos pos = entity.getPos();
+        BlockState state = world.getBlockState(pos);
+
+        BlockRenderManager manager = MinecraftClient.getInstance().getBlockRenderManager();
+        BakedModel model = manager.getModel(state);
+
+        //block itself
         matrices.push();
-        matrices.translate(0.5f,item.getItem() instanceof BlockItem ? 0.355f:0.17f,0.5f);
+        matrices.translate(0,0,0);
+        matrices.scale(1f,1f,1f);
+        manager.getModelRenderer().render(world,model,state,pos,matrices,vertexConsumers.getBuffer(RenderLayer.getSolid()),false, world.getRandom(),getLightLevel(world, pos),1);
+        matrices.pop();
+
+        // the item
+        matrices.push();
+        matrices.translate(0.5f,item.getItem() instanceof BlockItem ? 0.255f:0.07f,0.5f);
 
         matrices.scale(0.4f,0.4f,0.4f);
         if(!(item.getItem() instanceof BlockItem))matrices.multiply(RotationAxis.NEGATIVE_X.rotationDegrees(270));
 
-        itemRenderer.renderItem(item, item.getItem() instanceof BlockItem ? ModelTransformationMode.NONE : ModelTransformationMode.GUI,getLightLevel(entity.getWorld(),entity.getPos()), OverlayTexture.DEFAULT_UV,matrices,
-                vertexConsumers,entity.getWorld(),1);
+        itemRenderer.renderItem(item, item.getItem() instanceof BlockItem ? ModelTransformationMode.NONE : ModelTransformationMode.GUI,getLightLevel(world, pos), OverlayTexture.DEFAULT_UV,matrices,
+                vertexConsumers, world,1);
         matrices.pop();
 
     }
