5fada36a0ee3cecebe40ab163ecf7a9e1098998f
efekos <56752400+efekos@users.noreply.github.com>
2024-09-14 02:45:16 +0300

    add a custom renderer for PlateBlockEntity to render items

---
+++ src/main/java/dev/efekos/cla/block/entity/PlateBlockEntityRenderer.java
@@ -0,0 +1,63 @@
+package dev.efekos.cla.block.entity;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.BlockRenderManager;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.world.LightType;
+import net.minecraft.world.World;
+
+public class PlateBlockEntityRenderer implements BlockEntityRenderer<PlateBlockEntity> {
+
+    private final ItemRenderer itemRenderer;
+
+    @Override
+    public void render(PlateBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+
+        World world = entity.getWorld();
+        BlockPos pos = entity.getPos();
+        BlockState state = world.getBlockState(pos);
+
+        BlockRenderManager manager = MinecraftClient.getInstance().getBlockRenderManager();
+        BakedModel model = manager.getModel(state);
+
+        //block itself
+        matrices.push();
+        matrices.translate(0, 0, 0);
+        matrices.scale(1f, 1f, 1f);
+        VertexConsumer solid = vertexConsumers.getBuffer(RenderLayer.getSolid());
+        int lightLevel = getLightLevel(world, pos);
+        manager.getModelRenderer().render(world, model, state, pos, matrices, solid, false, world.getRandom(), lightLevel, 1);
+        matrices.pop();
+
+        for (ItemStack item : entity.getItems()) {
+            matrices.push();
+            matrices.translate(0.45f, 0.3f, 0.45f);
+            matrices.scale(0.4f,0.4f, 0.4f);
+            matrices.multiply(RotationAxis.NEGATIVE_X.rotationDegrees(270));
+            itemRenderer.renderItem(item, ModelTransformationMode.NONE,getLightLevel(world,pos),0,matrices,vertexConsumers,world,1);
+            matrices.pop();
+        }
+
+    }
+
+    private int getLightLevel(World world, BlockPos pos) {
+        return LightmapTextureManager.pack(world.getLightLevel(LightType.BLOCK, pos), world.getLightLevel(LightType.SKY, pos));
+    }
+
+    public PlateBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
+        this.itemRenderer = context.getItemRenderer();
+    }
+}
--- src/main/java/dev/efekos/cla/client/MainClient.java
+++ src/main/java/dev/efekos/cla/client/MainClient.java
@@ -1,9 +1,11 @@
 package dev.efekos.cla.client;
 
-import dev.efekos.cla.block.entity.CuttingBoardBlockEntity;
 import dev.efekos.cla.block.entity.CuttingBoardBlockEntityRenderer;
+import dev.efekos.cla.block.entity.PlateBlockEntityRenderer;
+import dev.efekos.cla.block.entity.SyncAbleBlockEntity;
 import dev.efekos.cla.init.ClaBlocks;
 import dev.efekos.cla.packet.CuttingBoardSyncS2C;
+import dev.efekos.cla.packet.PlateSyncS2C;
 import dev.efekos.cla.packet.RequestSyncC2S;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientBlockEntityEvents;
@@ -17,12 +19,14 @@ public class MainClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BlockEntityRendererFactories.register(ClaBlocks.CUTTING_BOARD_BLOCK_ENTITY_TYPE, CuttingBoardBlockEntityRenderer::new);
+        BlockEntityRendererFactories.register(ClaBlocks.PLATE_BLOCK_ENTITY_TYPE, PlateBlockEntityRenderer::new);
         ClientPlayNetworking.registerGlobalReceiver(CuttingBoardSyncS2C.PAYLOAD_ID,CuttingBoardSyncS2C::handle);
+        ClientPlayNetworking.registerGlobalReceiver(PlateSyncS2C.PAYLOAD_ID,PlateSyncS2C::handle);
         ClientBlockEntityEvents.BLOCK_ENTITY_LOAD.register(this::loadBlockEntity);
     }
 
     private void loadBlockEntity(BlockEntity blockEntity, ClientWorld clientWorld) {
-        if(blockEntity instanceof CuttingBoardBlockEntity) ClientPlayNetworking.send(new RequestSyncC2S(blockEntity.getPos()));
+        if(blockEntity instanceof SyncAbleBlockEntity) ClientPlayNetworking.send(new RequestSyncC2S(blockEntity.getPos()));
     }
 
 }
