dcda464b2deb4d54ceadeafbf49f2d2aba4e910a
efekos <56752400+efekos@users.noreply.github.com>
2024-09-16 00:16:28 +0300

    add cooking and plate pickup sounds

--- src/main/java/dev/efekos/cla/init/ClaSoundEvents.java
+++ src/main/java/dev/efekos/cla/init/ClaSoundEvents.java
@@ -9,6 +9,8 @@ public class ClaSoundEvents {
 
     public static final SoundEvent KNIFE_SLICE = of("item.knife.slice");
     public static final SoundEvent PLATE_PLACE = of("block.plate.place");
+    public static final SoundEvent PAN_COOKING = of("block.pan.cook");
+    public static final SoundEvent PLATE_PICKUP = of("block.plate.pickup");
 
     private static @NotNull SoundEvent of(String path) {
         return SoundEvent.of(Identifier.of(Main.MOD_ID, path));
