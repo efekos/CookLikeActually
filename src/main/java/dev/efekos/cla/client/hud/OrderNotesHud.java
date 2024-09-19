a26c17710ef356ef54592213019c7d8c4d9d5cc7
efekos <56752400+efekos@users.noreply.github.com>
2024-09-19 18:06:38 +0300

    add order note rendering

--- src/main/java/dev/efekos/cla/client/hud/OrderNotesHud.java
+++ src/main/java/dev/efekos/cla/client/hud/OrderNotesHud.java
@@ -3,6 +3,9 @@ package dev.efekos.cla.client.hud;
 import dev.efekos.cla.Main;
 import dev.efekos.cla.init.ClaComponentTypes;
 import dev.efekos.cla.init.ClaItems;
+import dev.efekos.cla.resource.Course;
+import dev.efekos.cla.resource.CourseManager;
+import dev.efekos.cla.util.CachedFunction;
 import dev.efekos.cla.util.IDrawContextMixin;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
@@ -16,11 +19,19 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class OrderNotesHud implements HudRenderCallback {
 
     public static final Identifier NOTE_TEXTURE = Identifier.of(Main.MOD_ID,"hud/order_note");
     public static final Identifier SLOT_TEXTURE = Identifier.of(Main.MOD_ID,"hud/order_note_ingredient");
+
+    private static final CachedFunction<Identifier,ItemStack> courseIdToStack = new CachedFunction<>(identifier -> new ItemStack(RegistryEntry.of(ClaItems.PLATE),1,ComponentChanges.builder().add(ClaComponentTypes.COURSE_ID,identifier).build()));
+    private static final CachedFunction<Identifier, List<ItemStack>> courseIdToIngredients = new CachedFunction<>(identifier -> {
+        Optional<Course> course = CourseManager.getInstance().getCourse(identifier);
+        return course.map(value -> value.ingredients().stream().map(ingredient -> ingredient.getMatchingStacks()[0]).toList()).orElseGet(ArrayList::new);
+    });
 
     @Override
     public void onHudRender(DrawContext drawContext, RenderTickCounter tickCounter) {
@@ -37,12 +48,25 @@ public class OrderNotesHud implements HudRenderCallback {
         }
 
         for (int i = 0; i < stacks.size(); i++) {
+            int x = i * 80 + 10;
+            drawContext.drawGuiTexture(NOTE_TEXTURE, x-1,0,71,100);
+
             ItemStack noteStack = stacks.get(i);
             Identifier courseId = noteStack.get(ClaComponentTypes.COURSE_ID);
-            drawContext.drawGuiTexture(NOTE_TEXTURE,i*80+10,0,70,100);
-            ItemStack plateStack = new ItemStack(RegistryEntry.of(ClaItems.PLATE), 1, ComponentChanges.builder().add(ClaComponentTypes.COURSE_ID, courseId).build());
-            IDrawContextMixin context = (IDrawContextMixin) drawContext;
-            context.cla$drawItemWithScale(plateStack,35,4,32f);
+            if(courseId==null)continue;
+
+            ItemStack plateStack = courseIdToStack.apply(courseId);
+            ((IDrawContextMixin) drawContext).cla$drawItemWithScale(plateStack,x+26,4,32f);
+
+            List<ItemStack> ingredientStacks = courseIdToIngredients.apply(courseId);
+            int startX = x + 33 - Math.min(4,ingredientStacks.size()) * 8;
+            for (int j = 0; j < ingredientStacks.size(); j++) {
+                ItemStack stack = ingredientStacks.get(j);
+                int x1 = startX + (j%4)*17;
+                int y = ((int)Math.floor(j/4d))*17+36;
+                drawContext.drawGuiTexture(SLOT_TEXTURE,x1,y,16,16);
+                ((IDrawContextMixin) drawContext).cla$drawItemWithScale(stack, x1,y,12f);
+            }
         }
 
     }
---
+++ src/main/java/dev/efekos/cla/util/CachedFunction.java
@@ -0,0 +1,28 @@
+package dev.efekos.cla.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class CachedFunction<T,R> {
+
+    private final Function<T,R> func;
+    private final Map<Integer,R> cache = new HashMap<>();
+
+    public CachedFunction(Function<T, R> func) {
+        this.func = func;
+    }
+
+    public R apply(T t) {
+        int i = t.hashCode();
+        if(cache.containsKey(i)) return cache.get(i);
+        R r = func.apply(t);
+        cache.put(i,r);
+        return r;
+    }
+
+    public void clear(){
+        cache.clear();
+    }
+
+}
