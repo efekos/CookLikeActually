f3cea3729726efec55e9073df3a63dc522ad349a
efekos <56752400+efekos@users.noreply.github.com>
2024-09-19 20:54:16 +0300

    move payload types to a different class

--- src/main/java/dev/efekos/cla/Main.java
+++ src/main/java/dev/efekos/cla/Main.java
@@ -23,14 +23,7 @@ public class Main implements ModInitializer {
         ClaGroups.run();
         ClaRecipeTypes.run();
         ClaSoundEvents.run();
-
-        // Payloads
-        PayloadTypeRegistry.playS2C().register(CuttingBoardSyncS2C.PAYLOAD_ID, CuttingBoardSyncS2C.CODEC);
-        PayloadTypeRegistry.playS2C().register(PlateSyncS2C.PAYLOAD_ID, PlateSyncS2C.CODEC);
-        PayloadTypeRegistry.playS2C().register(PanSyncS2C.PAYLOAD_ID, PanSyncS2C.CODEC);
-        PayloadTypeRegistry.playS2C().register(ItemBoxSyncS2C.PAYLOAD_ID, ItemBoxSyncS2C.CODEC);
-        PayloadTypeRegistry.playS2C().register(FryingStandSyncS2C.PAYLOAD_ID, FryingStandSyncS2C.CODEC);
-        PayloadTypeRegistry.playC2S().register(RequestSyncC2S.PAYLOAD_ID, RequestSyncC2S.CODEC);
+        ClaPayloadTypes.run();
 
         // Server Payload Listeners
         ServerPlayNetworking.registerGlobalReceiver(RequestSyncC2S.PAYLOAD_ID, RequestSyncC2S::handle);
---
+++ src/main/java/dev/efekos/cla/init/ClaPayloadTypes.java
@@ -0,0 +1,44 @@
+package dev.efekos.cla.init;
+
+import dev.efekos.cla.packet.*;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+
+public class ClaPayloadTypes {
+
+    public static final CustomPayload.Type<? super RegistryByteBuf, CuttingBoardSyncS2C> CUTTING_BOARD_SYNC_S2C = registers2c(CuttingBoardSyncS2C.class);
+    public static final CustomPayload.Type<? super RegistryByteBuf, PlateSyncS2C> PLATE_SYNC = registers2c(PlateSyncS2C.class);
+    public static final CustomPayload.Type<? super RegistryByteBuf, PanSyncS2C> PAN_SYNC_S2C = registers2c(PanSyncS2C.class);
+    public static final CustomPayload.Type<? super RegistryByteBuf, ItemBoxSyncS2C> ITEM_BOX_SYNC_S2C = registers2c(ItemBoxSyncS2C.class);
+    public static final CustomPayload.Type<? super RegistryByteBuf, FryingStandSyncS2C> FRYING_STAND_SYNC_S2C = registers2c(FryingStandSyncS2C.class);
+    public static final CustomPayload.Type<? super RegistryByteBuf, RequestSyncC2S> REQUEST_SYNC_C2S = registerc2s(RequestSyncC2S.class);
+
+    @SuppressWarnings("unchecked")
+    private static <T extends CustomPayload> CustomPayload.Type<? super RegistryByteBuf, T> registerc2s(Class<T> clazz) {
+        try {
+            PacketCodec<RegistryByteBuf, T> codec = (PacketCodec<RegistryByteBuf, T>) clazz.getDeclaredField("CODEC").get(null);
+            CustomPayload.Id<T> id = (CustomPayload.Id<T>) clazz.getDeclaredField("PAYLOAD_ID").get(null);
+            return PayloadTypeRegistry.playC2S().register(id, codec);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends CustomPayload> CustomPayload.Type<? super RegistryByteBuf, T> registers2c(Class<T> clazz) {
+        try {
+            PacketCodec<RegistryByteBuf, T> codec = (PacketCodec<RegistryByteBuf, T>) clazz.getDeclaredField("CODEC").get(null);
+            CustomPayload.Id<T> id = (CustomPayload.Id<T>) clazz.getDeclaredField("PAYLOAD_ID").get(null);
+            return PayloadTypeRegistry.playS2C().register(id, codec);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void run() {
+
+    }
+
+}
