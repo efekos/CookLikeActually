a7728e24792ee9c99184a2781612d6ad69adb8ae
efekos <56752400+efekos@users.noreply.github.com>
2024-09-20 07:13:56 +0300

    convert lettuce to a crop to make it farmable

---
+++ src/main/java/dev/efekos/cla/block/LettuceBlock.java
@@ -0,0 +1,41 @@
+package dev.efekos.cla.block;
+
+import com.mojang.serialization.MapCodec;
+import dev.efekos.cla.init.ClaItems;
+import net.minecraft.block.*;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+
+public class LettuceBlock extends CropBlock {
+
+    public static final MapCodec<LettuceBlock> CODEC = createCodec(LettuceBlock::new);
+    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 3.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 3.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 11.0, 16.0)
+    };
+
+    public LettuceBlock(Settings settings) {
+        super(settings);
+    }
+
+    public MapCodec<LettuceBlock> getCodec() {
+        return CODEC;
+    }
+
+    protected ItemConvertible getSeedsItem() {
+        return ClaItems.LETTUCE;
+    }
+
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[this.getAge(state)];
+    }
+
+}
