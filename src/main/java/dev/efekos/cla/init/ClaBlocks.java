26176ed6b018602bce626450fdfc7a517f78832a
efekos <56752400+efekos@users.noreply.github.com>
2024-09-21 12:45:39 +0300

    rename lettuce crop to lettuces

--- src/main/java/dev/efekos/cla/init/ClaBlocks.java
+++ src/main/java/dev/efekos/cla/init/ClaBlocks.java
@@ -21,7 +21,7 @@ public class ClaBlocks {
     public static final ItemBoxBlock ITEM_BOX = register("item_box", new ItemBoxBlock(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)));
     public static final TrashCanBlock TRASH_CAN = register("trash_can", new TrashCanBlock(AbstractBlock.Settings.copy(Blocks.DIRT).mapColor(MapColor.GRAY).sounds(BlockSoundGroup.WOOD)));
     public static final CropBlock TOMATOES = register("tomatoes", new TomatoesBlock(AbstractBlock.Settings.copy(Blocks.CARROTS).mapColor(MapColor.DULL_RED)));
-    public static final LettuceBlock LETTUCE = register("lettuce", new LettuceBlock(AbstractBlock.Settings.copy(Blocks.CARROTS).mapColor(MapColor.GREEN)));
+    public static final LettuceBlock LETTUCES = register("lettuces", new LettuceBlock(AbstractBlock.Settings.copy(Blocks.CARROTS).mapColor(MapColor.GREEN)));
     private static final AbstractBlock.Settings STAND_BLOCK_SETTINGS = AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).hardness(2.5f).resistance(3).mapColor(MapColor.PURPLE);
     public static final Block COOKING_STAND = register("cooking_stand", new Block(STAND_BLOCK_SETTINGS));
     public static final Block FRYING_STAND = register("frying_stand", new FryingStandBlock(STAND_BLOCK_SETTINGS.nonOpaque()));
