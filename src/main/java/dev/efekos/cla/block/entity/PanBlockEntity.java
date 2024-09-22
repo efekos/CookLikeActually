46bfc5fc041e9ed819b0943da2646d0def08c2aa
efekos <56752400+efekos@users.noreply.github.com>
2024-09-22 04:33:25 +0300

    add flame particles to pans

--- src/main/java/dev/efekos/cla/block/entity/PanBlockEntity.java
+++ src/main/java/dev/efekos/cla/block/entity/PanBlockEntity.java
@@ -10,12 +10,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.component.ComponentMap;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.recipe.input.SingleStackRecipeInput;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class PanBlockEntity extends BlockEntityWithOneItem implements SyncAbleBlockEntity<PanSyncS2C> {
 
@@ -49,11 +54,28 @@ public class PanBlockEntity extends BlockEntityWithOneItem implements SyncAbleBl
             world.playSound(pos.getX(), pos.getY(), pos.getZ(), ClaSoundEvents.PAN_COOKING, SoundCategory.BLOCKS, 1f, 1f, true);
             PanningRecipe recipe = getRecipe(world);
             if (getTicks() >= recipe.getTime()) {
+
+                if(world.isClient()) {
+                    Box box = new Box(new Vec3d(pos.getX()+.1, pos.getY() + .1, pos.getZ()+.1), new Vec3d(pos.getX() +.9, pos.getY() + .15, pos.getZ() +.9));
+                    for (int i = 0; i < 10; i++) {
+                        Vec3d p = findRandomPos(box);
+                        world.addParticle(ParticleTypes.FLAME.getType(), p.x, p.y, p.z, 0, 0, 0);
+                    }
+                }
+
                 setTicks(0);
                 setItem(recipe.getRes());
             }
             markDirty();
         } else if (getTicks() != 0) setTicks(0);
+    }
+
+    private Vec3d findRandomPos(Box box){
+        Random random = new Random();
+        double x = random.nextDouble(box.minX,box.maxX);
+        double y = random.nextDouble(box.minY,box.maxY);
+        double z = random.nextDouble(box.minZ,box.maxZ);
+        return new Vec3d(x,y,z);
     }
 
     public boolean hasRecipe(World world) {
